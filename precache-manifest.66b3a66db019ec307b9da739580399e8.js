936ed22b91bbece62c95b486c0a49011803e6421
Jgayle <Jgayle99@gmail.com>
2020-01-31 12:28:41 -0500

    Updates

---
+++ precache-manifest.66b3a66db019ec307b9da739580399e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a72791d06ff0f04c475b104bb80a37",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bea68da0e4b0544133a7",
+    "url": "/monsters-rolodex/static/css/main.35310252.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea68da0e4b0544133a7",
+    "url": "/monsters-rolodex/static/js/main.d8f2fbe9.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
